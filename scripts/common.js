a9da17d3508b3b30a522dfea5d0093f0d9e44ea6
Madhav Thotapalli <madhavmurthyt@gmail.com>
2020-10-02 12:47:51 +0530

    SignUp Done

---
+++ scripts/common.js
@@ -0,0 +1,35 @@
+
+
+
+function signUp() {
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("signUp");
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+
+}
+// Get the modal
+
+// Get the button that opens the modal
+
+// Get the <span> element that closes the modal
+//var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+
+
+// When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
+//
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
